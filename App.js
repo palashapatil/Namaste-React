7fed8926f49fd54002aa6f1806c67c7b3d9b67ae
Palash Patil <palashapatil@gmail.com>
2023-06-17 13:58:05 +0530

    JSX learning

--- App.js
+++ App.js
@@ -1,33 +1,22 @@
-/**
- * 
- * <div id="parent">
- *      <div id="child">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *      </div>
- * <div id="child2">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *      </div>
- * </div>
- * 
- * ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div", {id: "parent"},[
-    React.createElement("div", {id:"child"}, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", {id:"child2"}, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-]);
+// React Element
+const Title = () =>  (
+<h1 id="heading" tabIndex="5" >
+    Namaste React using JSX
+</h1>
+);
 
+// Functional Components - NEW
 
-console.log(parent);
+const HeadingComponent = () => ( 
+    <div id="container"> 
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+)
 
-const root = ReactDOM.createRoot(document.getElementById("header"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent);
+root.render(<HeadingComponent />)
